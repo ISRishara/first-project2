8a8ff75659223d3d7fec869948f7d1bb4c8ff919
Ishara <asusvivobook361lk@gmail.com>
2024-12-19 14:36:08 +0530

    create page.jsx inside login

---
+++ app/login/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function LoginPage() {
+    return(
+        <div className="flex flex-col justify-center items-center w-full min-h-screen bg-yellow-200 text-black text-center">
+            <div className="bg-green-300 px-12 py-5 rounded-lg"> Login </div>
+         </div>
+    )
+           
+}
+
