6f0d69ea9879975daf31305dae356d84ab8eda87
Ishara <asusvivobook361lk@gmail.com>
2025-02-05 14:38:34 +0530

    Movie creation part

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,3 +1,4 @@
+import SidePanel from "./components/side-panel";
 import UserNav from "./components/user-nav";
 
 export default function DashboardLayout({ children }) {
@@ -5,12 +6,12 @@ export default function DashboardLayout({ children }) {
     <div className="flex max-h-screen overflow-hidden bg-gray-100">
       {/*Side panel */}
       <aside className="w-64 overflow-y-auto border-r bg-white shadow-lg">
-        Side Panel
+        <SidePanel />
       </aside>
       <div className="bg-gray-500 flex flex-1 flex-col overflow-hidden">
         {/*Dashboard header */}
-        <header className="bg-white flex h-16 items-center justify-between gap-4 border-b px-6 shadow-sm">
-          <h1 className="text-2xl font-bold text-red-700 ">Netflix Dashboard</h1>
+        <header className="bg-red-900 flex h-16 items-center justify-between gap-4 border-b px-6 shadow-sm">
+          <h1 className="text-2xl font-bold text-white ">Netflix Dashboard</h1>
           <UserNav />
         </header>
 
